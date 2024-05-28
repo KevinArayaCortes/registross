31a62eb5f3b50afd4ef9e033290dffce4c3ba642
KevinArayaCortes <kevin2401araya@gmail.com>
2024-05-28 09:47:43 -0400

    a
    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,26 @@ window.addEventListener("load",()=>{
 })
 
 const registrar = ()=>{
-    //recuperar elemento
-    let eNombre = document.getElementById("nombre");
-    //recupero valor de elemento
-    let vNombre = eNombre.value;
-    //crea un objeto con los datos recuperados
-    let objeto = {nombre:vNombre}
-    //envio a una funcion que registra
-    console.log(objeto)
-    registrarPersona(objeto).then(()=>{
-        alert("Se registra con existo");
-    }).catch((error)=>{
-        console.log(error);
-    });
+//recuperar elemento
+let eNombre = document.getElementById("nombre");
+let eApellido = document.getElementById("apellido");
+let eRut = document.getElementById("rut");
+let eCorreo = document.getElementById("correo");
+let ephone = document.getElementById("phone");
+let eEdad = document.getElementById("edad");
+//recupero valor de elemento
+let vNombre = eNombre.value;
+let vApellido= eApellido.value;
+let vRut = eRut.value;
+let vCorreo = eCorreo.value;
+let vphone = ephone.value;
+let vEdad = eEdad.value;
+//crea un objeto con los datos recuperados
+let objeto = {nombre:vNombre,apellido:vApellido,rut:vRut,correo:vCorreo,phone:vphone,edad:vEdad}
+//envio a una funcion que registra
+registrarPersona(objeto).then(()=>{
+alert("Se registro exitosamente");
+}).catch((error)=>{
+console.log(error)
+});
 }
