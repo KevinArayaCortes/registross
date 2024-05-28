6f0780dd7f032f3338c0986ff482f559d9bf7845
KevinArayaCortes <kevin2401araya@gmail.com>
2024-05-28 11:38:02 -0400

    a
    a

--- assets/js/promesa.js
+++ assets/js/promesa.js
@@ -1,5 +1,19 @@
 import { db } from "./firebase.js";
-import { addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { addDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 export const registrarPersona = async(persona)=>{
     const docRef = await addDoc(collection(db, "personas"), persona);
+}
+
+export const obtenerPersonas = async()=>{
+    // Recupera la referencia (ruta)
+    const ref = collection(db, "personas");
+    const qSnap = await getDocs(ref);
+    let listado = []
+    qSnap.forEach((doc) => {
+        //console.log(doc.id)
+        console.log(doc.data())
+        listado.push({...doc.data(),id:doc.id})
+    });
+    console.log(listado)
+    return listado;
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-import { registrarPersona } from "./promesa.js";
+import { obtenerPersonas, registrarPersona } from "./promesa.js";
 window.addEventListener("load",()=>{
     document.getElementById("btnRegistrar").addEventListener("click",registrar);
+    cargarDatos();
 })
 
 const registrar = ()=>{
@@ -9,21 +10,58 @@ let eNombre = document.getElementById("nombre");
 let eApellido = document.getElementById("apellido");
 let eRut = document.getElementById("rut");
 let eCorreo = document.getElementById("correo");
-let ephone = document.getElementById("phone");
+let efdn = document.getElementById("fdn");
 let eEdad = document.getElementById("edad");
 //recupero valor de elemento
 let vNombre = eNombre.value;
 let vApellido= eApellido.value;
 let vRut = eRut.value;
 let vCorreo = eCorreo.value;
-let vphone = ephone.value;
+let vfdn = efdn.value;
 let vEdad = eEdad.value;
 //crea un objeto con los datos recuperados
-let objeto = {nombre:vNombre,apellido:vApellido,rut:vRut,correo:vCorreo,phone:vphone,edad:vEdad}
+let objeto = {nombre:vNombre,apellido:vApellido,rut:vRut,correo:vCorreo,fecha:vfdn,edad:vEdad}
 //envio a una funcion que registra
 registrarPersona(objeto).then(()=>{
 alert("Se registro exitosamente");
+cargarDatos();
 }).catch((error)=>{
 console.log(error)
 });
 }
+const cargarDatos = ()=>{
+    //Traer de las promesas todo lo registrado
+    obtenerPersonas().then((personas)=>{
+        console.log("HOLA")
+        console.log(personas)
+        //Cargarlo en la tabla del html
+        let estructuras = ""
+        personas.forEach((p)=>{
+            estructuras += "<tr>"
+            estructuras += "<td>"+p.nombre+"</td>"
+            estructuras += "<td>"+p.apellido+"</td>"
+            estructuras += "<td>"+p.rut+"</td>"
+            estructuras += "<td>"+p.correo+"</td>"
+            estructuras += "<td>"+p.edad+"</td>"
+            estructuras += "<td>"+p.fecha+"</td>"
+            estructuras += "<td><button id='UPD"+p.id+"'>Actualizar</button></td>"
+            estructuras += "<td><button id='DEL"+p.id+"'>Eliminar</button></td>"
+            estructuras += "</tr>";
+        })
+        document.getElementById("cuerpoTabla").innerHTML = estructuras;
+
+        personas.forEach((p)=>{
+            let elemento = document.getElementById("UPD"+p.id);
+            elemento.addEventListener("click",()=>{
+                document.getElementById("UPDnombre").value = p.nombre;
+                document.getElementById("UPDapellido").value = p.apellido;
+                document.getElementById("UPDrut").value = p.rut;
+                document.getElementById("UPDcorreo").value = p.correo;
+                document.getElementById("UPDedad").value = p.edad;
+                document.getElementById("UPDfdn").value = p.fecha;
+                document.getElementById("btnActualizar").value = p.id
+            })
+        })
+    })
+    
+};
